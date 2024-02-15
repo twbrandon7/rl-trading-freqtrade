2e8322edccc66bd6e3815d729b6a4517d5d55673
Li-Xian Chen <twbrandon7@gmail.com>
2024-02-15 04:55:05 +0000

    feat(rl): add basic files for reinforcement learning

---
+++ user_data/freqaimodels/CleanRlLearner.py
@@ -0,0 +1,123 @@
+from typing import Dict, Any, Union, Optional
+
+import torch
+import gymnasium as gym
+import numpy as np
+import pandas as pd
+from datasieve.pipeline import Pipeline
+from datasieve.transforms import SKLearnWrapper
+from sklearn.preprocessing import MinMaxScaler
+from torch import nn
+
+from freqtrade.freqai.RL.Base3ActionRLEnv import Actions, Base3ActionRLEnv, Positions
+from freqtrade.freqai.RL.BaseReinforcementLearningModel import BaseReinforcementLearningModel
+from freqtrade.freqai.data_kitchen import FreqaiDataKitchen
+from stable_baselines3.common.vec_env import SubprocVecEnv, VecMonitor
+from sb3_contrib.common.maskable.callbacks import MaskableEvalCallback
+from pandas import DataFrame
+
+
+class CleanRlLearner(BaseReinforcementLearningModel):
+    def __init__(self, **kwargs) -> None:
+        super().__init__(config=kwargs["config"])
+        self.max_threads = min(
+            self.freqai_info["rl_config"].get("cpu_count", 1),
+            max(int(self.max_system_threads / 2), 1),
+        )
+        torch.set_num_threads(self.max_threads)
+        self.reward_params = self.freqai_info["rl_config"]["model_reward_parameters"]
+        self.train_env: Union[VecMonitor, SubprocVecEnv, gym.Env] = gym.Env()
+        self.eval_env: Union[VecMonitor, SubprocVecEnv, gym.Env] = gym.Env()
+        self.eval_callback: Optional[MaskableEvalCallback] = None
+        self.model_type = self.freqai_info["rl_config"]["model_type"]
+        self.rl_config = self.freqai_info["rl_config"]
+        self.df_raw: DataFrame = DataFrame()
+        self.continual_learning = self.freqai_info.get("continual_learning", False)
+
+        self.unset_outlier_removal()
+        self.dd.model_type = "pytorch"
+
+    def fit(self, data_dictionary: Dict[str, Any], dk: FreqaiDataKitchen, **kwargs):
+        env = self.train_env
+        next_obs, _ = env.reset(seed=42)
+        max_steps = self.config["rl_config"]["max_trade_duration_candles"]
+        for step in range(0, max_steps - 1):
+            next_obs, reward, terminations, truncations, infos = env.step(0)
+            pass
+
+    # def define_data_pipeline(self, threads=-1) -> Pipeline:
+    #     pipe_steps = [
+    #         # ("const", ds.VarianceThreshold(threshold=0)),
+    #         # ("scaler", SKLearnWrapper(MinMaxScaler(feature_range=(-1, 1)))),
+    #     ]
+    #     return Pipeline(pipe_steps)
+
+    class MyRLEnv(Base3ActionRLEnv):
+        def _get_observation(self):
+            """
+            This may or may not be independent of action types, user can inherit
+            this in their custom "MyRLEnv"
+            """
+            features_window = self.signal_features[
+                (self._current_tick - self.window_size) : self._current_tick
+            ]
+            if self.add_state_info:
+                features_and_state = DataFrame(
+                    np.zeros((len(features_window), 3)),
+                    columns=["current_profit_pct", "position", "trade_duration"],
+                    index=features_window.index,
+                )
+
+                features_and_state["current_profit_pct"] = self.get_unrealized_profit()
+                features_and_state["position"] = self._position.value
+                features_and_state["trade_duration"] = self.get_trade_duration()
+                features_and_state = pd.concat([features_window, features_and_state], axis=1)
+                return features_and_state
+            else:
+                return features_window
+
+        def calculate_reward(self, action: int) -> float:
+
+            # first, penalize if the action is not valid
+            if not self._is_valid(action):
+                return -2
+
+            pnl = self.get_unrealized_profit()
+            rew = np.sign(pnl) * (pnl + 1)
+            factor = 100.0
+
+            # reward agent for entering trades
+            if (
+                action in (Actions.Buy.value, Actions.Sell.value)
+                and self._position == Positions.Neutral
+            ):
+                return 25
+            # discourage agent from not entering trades
+            if action == Actions.Neutral.value and self._position == Positions.Neutral:
+                return -1
+
+            max_trade_duration = self.rl_config.get("max_trade_duration_candles", 300)
+            trade_duration = self._current_tick - self._last_trade_tick  # type: ignore
+
+            if trade_duration <= max_trade_duration:
+                factor *= 1.5
+            elif trade_duration > max_trade_duration:
+                factor *= 0.5
+
+            # discourage sitting in position
+            if self._position in (Positions.Short, Positions.Long) and (
+                action == Actions.Neutral.value
+                or (action == Actions.Sell.value and self._position == Positions.Short)
+                or (action == Actions.Buy.value and self._position == Positions.Long)
+            ):
+                return -1 * trade_duration / max_trade_duration
+
+            # close position
+            if (action == Actions.Buy.value and self._position == Positions.Short) or (
+                action == Actions.Sell.value and self._position == Positions.Long
+            ):
+                if pnl > self.profit_aim * self.rr:
+                    factor *= self.rl_config["model_reward_parameters"].get("win_reward_factor", 2)
+                return float(rew * factor)
+
+            return 0.0
---
+++ user_data/strategies/rl_strategy.py
@@ -0,0 +1,128 @@
+import numpy as np  # noqa
+import pandas as pd  # noqa
+from pandas import DataFrame
+from typing import Optional, Union
+from functools import reduce
+
+from freqtrade.freqai.RL.Base3ActionRLEnv import Actions
+from freqtrade.strategy import (
+    BooleanParameter,
+    CategoricalParameter,
+    DecimalParameter,
+    IStrategy,
+    IntParameter,
+)
+
+import talib.abstract as ta
+import freqtrade.vendor.qtpylib.indicators as qtpylib
+
+
+class MyRLStrategy(IStrategy):
+    # user should define the maximum startup candle count (the largest number of candles
+    # passed to any single indicator)
+    startup_candle_count: int = 20
+
+    stoploss = -0.10
+
+    plot_config = {
+        "main_plot": {},
+        "subplots": {
+            "prediction": {"prediction": {"color": "blue"}},
+            "do_predict": {
+                "do_predict": {"color": "brown"},
+            },
+        },
+    }
+
+    def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        # the model will return all labels created by user in `set_freqai_targets()`
+        # (& appended targets), an indication of whether or not the prediction should be accepted,
+        # the target mean/std values for each of the labels created by user in
+        # `set_freqai_targets()` for each training period.
+
+        dataframe = self.freqai.start(dataframe, metadata, self)
+        return dataframe
+
+    def set_freqai_targets(self, dataframe, **kwargs) -> DataFrame:
+        """
+        *Only functional with FreqAI enabled strategies*
+        Required function to set the targets for the model.
+        All targets must be prepended with `&` to be recognized by the FreqAI internals.
+
+        More details about feature engineering available:
+
+        https://www.freqtrade.io/en/latest/freqai-feature-engineering
+
+        :param df: strategy dataframe which will receive the targets
+        usage example: dataframe["&-target"] = dataframe["close"].shift(-1) / dataframe["close"]
+        """
+        # For RL, there are no direct targets to set. This is filler (neutral)
+        # until the agent sends an action.
+        dataframe["&-action"] = 0
+        return dataframe
+
+    def feature_engineering_standard(self, dataframe: DataFrame, **kwargs) -> DataFrame:
+        """
+        *Only functional with FreqAI enabled strategies*
+        This optional function will be called once with the dataframe of the base timeframe.
+        This is the final function to be called, which means that the dataframe entering this
+        function will contain all the features and columns created by all other
+        freqai_feature_engineering_* functions.
+
+        This function is a good place to do custom exotic feature extractions (e.g. tsfresh).
+        This function is a good place for any feature that should not be auto-expanded upon
+        (e.g. day of the week).
+
+        All features must be prepended with `%` to be recognized by FreqAI internals.
+
+        :param df: strategy dataframe which will receive the features
+        usage example: dataframe["%-day_of_week"] = (dataframe["date"].dt.dayofweek + 1) / 7
+        """
+
+        # The following features are necessary for RL models
+        dataframe[f"%-raw_close"] = dataframe["close"]
+        dataframe[f"%-raw_open"] = dataframe["open"]
+        dataframe[f"%-raw_high"] = dataframe["high"]
+        dataframe[f"%-raw_low"] = dataframe["low"]
+
+        dataframe[f"%-indi_adx"] = ta.ADX(dataframe)
+        dataframe[f"%-indi_rsi"] = ta.RSI(dataframe)
+
+        return dataframe
+
+    def populate_entry_trend(self, df: DataFrame, metadata: dict) -> DataFrame:
+        enter_long_conditions = [
+            df["do_predict"] == 1,
+            df["&-action"] == Actions.Buy.value and df['enter_tag'] != "long" and df['enter_tag'] != "short",
+        ]
+        if enter_long_conditions:
+            df.loc[
+                reduce(lambda x, y: x & y, enter_long_conditions), ["enter_long", "enter_tag"]
+            ] = (1, "long")
+
+        enter_short_conditions = [
+            df["do_predict"] == 1,
+            df["&-action"] == Actions.Sell.value and df['enter_tag'] != "long" and df['enter_tag'] != "short",
+        ]
+        if enter_short_conditions:
+            df.loc[
+                reduce(lambda x, y: x & y, enter_short_conditions), ["enter_short", "enter_tag"]
+            ] = (1, "short")
+
+        return df
+
+    def populate_exit_trend(self, df: DataFrame, metadata: dict) -> DataFrame:
+        exit_long_conditions = [
+            df["do_predict"] == 1,
+            df["&-action"] == Actions.Sell.value and df['enter_tag'] == "long",
+        ]
+        if exit_long_conditions:
+            df.loc[reduce(lambda x, y: x & y, exit_long_conditions), "exit_long"] = 1
+
+        exit_short_conditions = [
+            df["do_predict"] == 1, df["&-action"] == Actions.Buy.value and df['enter_tag'] == "short"
+        ]
+        if exit_short_conditions:
+            df.loc[reduce(lambda x, y: x & y, exit_short_conditions), "exit_short"] = 1
+
+        return df
